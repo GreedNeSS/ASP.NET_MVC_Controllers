49dede5910f54947626473d80bf919502459ceaf
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:15:33 +0300

    Fixed route.

--- ContentResult_JsonResult/Program.cs
+++ ContentResult_JsonResult/Program.cs
@@ -3,6 +3,5 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute("default", "{controller=Home}/{action=Json}/{Id?}");
 
 app.Run();
