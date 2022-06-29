94642e538431fd907e610cf1ae94146d95adf949
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:19:33 +0300

    Fixed route.

--- Files/Program.cs
+++ Files/Program.cs
@@ -3,8 +3,5 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute("file", "{controller=Home}/{action=GetFile}/{Id?}");
-app.MapControllerRoute("bytes", "{controller=Home}/{action=GetBytes}/{Id?}");
-app.MapControllerRoute("stream", "{controller=Home}/{action=GetStream}/{Id?}");
 
 app.Run();
