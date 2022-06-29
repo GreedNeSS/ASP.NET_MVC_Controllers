1f36764e042ae492d2e21059b31881eadb848f7c
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:25:28 +0300

    Fixed route.

--- StatusCode/Program.cs
+++ StatusCode/Program.cs
@@ -3,7 +3,5 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute("default", "{controller=Home}/{action=Auth}/{Id?}");
-app.MapControllerRoute("default", "{controller=Home}/{action=NotFound}/{Id?}");
 
 app.Run();
