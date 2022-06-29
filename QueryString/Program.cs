7e3886b85d4cb0416f2a22f10e11b528f79ad443
GreedNeSS <ruslan199126@gmail.com>
2022-06-29 18:21:14 +0300

    Fixed route.

--- QueryString/Program.cs
+++ QueryString/Program.cs
@@ -3,9 +3,5 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{Id?}");
-app.MapControllerRoute(name: "Person", pattern: "{controller=Home}/{action=Person}/{Id?}");
-app.MapControllerRoute(name: "Array", pattern: "{controller=Home}/{action=Array}/{Id?}");
-app.MapControllerRoute(name: "ArrayPersons", pattern: "{controller=Home}/{action=ArrayPersons}/{Id?}");
-app.MapControllerRoute(name: "Dictionary", pattern: "{controller=Home}/{action=Dictionary}/{Id?}");
 
 app.Run();
